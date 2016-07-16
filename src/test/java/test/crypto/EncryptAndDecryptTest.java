7cd3256984014107421b622928b3a339be15ec45
Junlapong L <junlapong@gmail.com>
2016-07-16 15:45:11 +0700

    add maven wrapper

--- src/test/java/test/crypto/EncryptAndDecryptTest.java
+++ src/test/java/test/crypto/EncryptAndDecryptTest.java
@@ -11,11 +11,12 @@ import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.util.Collection;
 import java.util.Iterator;
-
 import org.bouncycastle.cms.CMSAlgorithm;
+import org.bouncycastle.cms.CMSEnvelopedData;
+import org.bouncycastle.cms.CMSEnvelopedDataGenerator;
 import org.bouncycastle.cms.CMSEnvelopedDataParser;
-import org.bouncycastle.cms.CMSEnvelopedDataStreamGenerator;
-import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.cms.CMSProcessableByteArray;
+import org.bouncycastle.cms.CMSTypedData;
 import org.bouncycastle.cms.Recipient;
 import org.bouncycastle.cms.RecipientInformation;
 import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
@@ -39,15 +40,14 @@ public class EncryptAndDecryptTest {
 	//private static final String WORK_DIR = "D:\\git-space\\cms-decrypt-example";
 	private static final String WORK_DIR = "/home/junlapong/git-space/cms-decrypt-example";
 
-	private static final File SOURCE_PDF = new File(WORK_DIR, "source.xml");
+	private static final File SOURCE_FILE = new File(WORK_DIR, "source.xml");
 	private static final File DESTINATION_FILE = new File(WORK_DIR, "encrypted.xml");
 	private static final File DECRYPTED_FILE = new File(WORK_DIR, "decrypted.xml");
 
 	public static final String SIGNATURE_ALGORITHM = "SHA1withRSA";
 
 	@Test
-	@Ignore
-	public void shouldEncryptAndDecrypt() throws Exception {
+	public void shouldEncryptAndDecryptFile() throws Exception {
 
 		if (!new File(WORK_DIR).exists()) {
 			throw new RuntimeException("Update WORK_DIR to point to the directory the project is cloned into.");
@@ -59,60 +59,57 @@ public class EncryptAndDecryptTest {
 		X509Certificate certificate = EncryptAndDecrypt.getX509Certificate(new File(WORK_DIR, "certificate.pem"));
 		PrivateKey privateKey = EncryptAndDecrypt.getPrivateKey(new File(WORK_DIR, "certificate.p12"), "Qwer12345");
 
-		EncryptAndDecrypt.encrypt(certificate, SOURCE_PDF, DESTINATION_FILE);
+		EncryptAndDecrypt.encrypt(certificate, SOURCE_FILE, DESTINATION_FILE);
 		EncryptAndDecrypt.decrypt(privateKey, DESTINATION_FILE, DECRYPTED_FILE);
 	}
 
 	@Test
-	public void shouldEncryptDecryptMessage() throws Exception {
+	public void shouldEncryptAndDecryptMessage() throws Exception {
 
-		X509Certificate publicCert = EncryptAndDecrypt.getX509Certificate(new File(WORK_DIR, "certificate.pem"));		
-		String encryptedMessage = encryptMessage("<XML>", publicCert);
-		
-		PrivateKey privateKey = EncryptAndDecrypt.getPrivateKey(new File(WORK_DIR, "certificate.p12"), "Qwer12345");
-		// decryptMessage(encryptedMessage, privateKey);
-		
-	}
-	
-	public static String encryptMessage(String message, X509Certificate cert) throws CertificateEncodingException, CMSException, IOException {
-
-		CMSEnvelopedDataStreamGenerator generator = new CMSEnvelopedDataStreamGenerator();
-		generator.addRecipientInfoGenerator(new JceKeyTransRecipientInfoGenerator(cert));
-		
-		OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES256_CBC).setProvider(BouncyCastleProvider.PROVIDER_NAME).build();
-
-		ByteArrayOutputStream signedData = new ByteArrayOutputStream();
-		OutputStream encryptingStream = generator.open(signedData, encryptor);
-
-		encryptingStream.write(message.getBytes("UTF-8"));			
-		String encryptedMessage = new String(Base64.encode(signedData.toByteArray()), "UTF-8");
-		
+		X509Certificate certificate = EncryptAndDecrypt.getX509Certificate(new File(WORK_DIR, "certificate.pem"));
+		String encryptedMessage = encryptMessage("<InitRequest></InitRequest>", certificate);
 		System.out.println(encryptedMessage);
-		
-		return encryptedMessage;
 
+		PrivateKey privateKey = EncryptAndDecrypt.getPrivateKey(new File(WORK_DIR, "certificate.p12"), "Qwer12345");
+		String decryptedMessage = decryptMessage(encryptedMessage, privateKey);
+		System.out.println(decryptedMessage);
 	}
-	
-	public static void decryptMessage(String encryptedMessage, PrivateKey privateKey) throws IOException, CMSException {
 
-		byte[] encryptedData = encryptedMessage.getBytes("UTF-8");
+	public static String encryptMessage(String message, X509Certificate cert) throws Exception {
 
+		// create CMS envelope data;
+		// check http://www.ietf.org/rfc/rfc3852.txt pages 15-16 for details
+		CMSEnvelopedDataGenerator gen = new CMSEnvelopedDataGenerator();
+		gen.addRecipientInfoGenerator(new JceKeyTransRecipientInfoGenerator(cert));
+
+		OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC).setProvider(BouncyCastleProvider.PROVIDER_NAME).build();
+		CMSTypedData content = new CMSProcessableByteArray(message.getBytes("UTF-8"));
+		CMSEnvelopedData data = gen.generate(content, encryptor);
+		String encryptedMessage = new String(Base64.encode(data.getEncoded()));
+
+		return encryptedMessage;
+	}
+
+	public static String decryptMessage(String encryptedMessage, PrivateKey privateKey) throws Exception {
+
+		byte[] encryptedData = Base64.decode(encryptedMessage);
+
+		// parse CMS envelope data
 		CMSEnvelopedDataParser parser = new CMSEnvelopedDataParser(encryptedData);
 
-		RecipientInformation recInfo = getSingleRecipient(parser);
-		Recipient recipient = new JceKeyTransEnvelopedRecipient(privateKey);
-		
-//		ByteArrayInputStream decryptedStream = (ByteArrayInputStream) recInfo.getContentStream(recipient).getContentStream();
-
-	}
-
-	private static RecipientInformation getSingleRecipient(CMSEnvelopedDataParser parser) {
 		Collection<RecipientInformation> recInfos = parser.getRecipientInfos().getRecipients();
 		Iterator<RecipientInformation> recipientIterator = recInfos.iterator();
-		if (!recipientIterator.hasNext()) {
+
+		if (! recipientIterator.hasNext()) {
 			throw new RuntimeException("Could not find recipient");
 		}
-		return (RecipientInformation) recipientIterator.next();
+
+		// retrieve recipient and decode it
+		RecipientInformation recInfo = (RecipientInformation) recipientIterator.next();
+		Recipient recipient = new JceKeyTransEnvelopedRecipient(privateKey);
+		byte[] decryptedData = recInfo.getContent(recipient);
+
+		return new String(decryptedData);
 	}
 
 }
